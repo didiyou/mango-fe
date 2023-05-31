24b47515cdb35d6ef24984eb22b479e6806deccd
dylan <1422554376@qq.com>
2023-05-31 23:07:07 +0800

    完成数字输入功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,39 +1,71 @@
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import s from './InputPad.module.scss'
 import { Icon } from '../../shared/Icon';
-
+import { DatetimePicker, Popup } from 'vant';
+import 'vant/lib/index.css';
+import { time } from '../../shared/time';
 export const InputPad = defineComponent({
-    setup:(props, context) =>{
-        const buttonMap = [
-            {text:'1',onClick:()=>{}},
-            {text:'2',onClick:()=>{}},
-            {text:'3',onClick:()=>{}},
-            {text:'清空',onClick:()=>{}},
-            {text:'4',onClick:()=>{}},
-            {text:'5',onClick:()=>{}},
-            {text:'6',onClick:()=>{}},
-            {text:'+',onClick:()=>{}},
-            {text:'7',onClick:()=>{}},
-            {text:'8',onClick:()=>{}},
-            {text:'9',onClick:()=>{}},
-            {text:'-',onClick:()=>{}},
-            {text:'.',onClick:()=>{}},
-            {text:'0',onClick:()=>{}},
-            {text:'删',onClick:()=>{}},
-            {text:'提交',onClick:()=>{}}
-        ]
-        return ()=>(
+    setup: (props, context) => {
+        const buttons = [
+            { text: '1', onClick: () => { appendText(1) } },
+            { text: '2', onClick: () => { appendText(2) } },
+            { text: '3', onClick: () => { appendText(3) } },
+            { text: '4', onClick: () => { appendText(4) } },
+            { text: '5', onClick: () => { appendText(5) } },
+            { text: '6', onClick: () => { appendText(6) } },
+            { text: '7', onClick: () => { appendText(7) } },
+            { text: '8', onClick: () => { appendText(8) } },
+            { text: '9', onClick: () => { appendText(9) } },
+            { text: '.', onClick: () => { appendText('.') } },
+            { text: '0', onClick: () => { appendText(0) } },
+            { text: '清空', onClick: () => { refAmount.value = '0' } },
+            { text: '提交', onClick: () => { } },
+          ]
+        let refAmount = ref('0')
+        let dateVisible = ref(false)
+        let refDate = ref(new Date())
+        const showDatePicker = ()=> dateVisible.value = true
+        const hideDatePicker = ()=> dateVisible.value = false
+        
+        
+        const appendText = (n:number|string)=> {
+            const nString = n.toString()
+            const dotIndex = refAmount.value.indexOf('.')
+            if(refAmount.value.length >13){return}
+            if(dotIndex > 0 && refAmount.value.length - dotIndex >2){return}
+            if(nString==='.'){
+                if(dotIndex===-1){refAmount.value += nString}
+            }
+            else if(nString==='0'){
+                if(refAmount.value !== '0'){refAmount.value += nString}
+            }else{
+                if(refAmount.value==='0'){
+                    refAmount.value = ''
+                }
+                refAmount.value += nString
+            }
+
+
+        }
+        return () => (
             <>
-            <div class={s.dateAndAmount}>
-                <span class={s.calendar}>
-                    <Icon name="calendar" class={s.icon}/>
-                    <span>2023-5-26</span>
-                </span>
-                <span class={s.amount}>100</span>
-            </div>
-            <div class={s.buttons}>
-                {buttonMap.map(button => <button onClick={button.onClick}>{button.text}</button>)}
-            </div>
+                <div class={s.dateAndAmount}>
+                    <span class={s.calendar}>
+                        <Icon name="calendar" class={s.icon} />
+                        <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+                        <Popup
+                            v-model:show={dateVisible.value}
+                            position='bottom'>
+                      <DatetimePicker value={refDate.value} title="选择日期" type="date" 
+                      onConfirm={(date:Date)=> {refDate.value = date; hideDatePicker}}
+                      onCancel={hideDatePicker}/>
+                    </Popup>
+                    </span>
+                    <span class={s.amount}>{refAmount.value}</span>
+                </div>
+                <div class={s.buttons}>
+                    {buttons.map(button => <button onClick={button.onClick}>{button.text}</button>)}
+                </div>
             </>
         )
     }
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from 'unplugin-vue-components/resolvers';
 // import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
 import { svgstore } from './src/vite_plugins/svgstore';
 // https://vitejs.dev/config/
@@ -11,6 +13,9 @@ export default defineConfig({
       mergeProps:true
     }),
     svgstore(),
+    Components({
+      resolvers: [VantResolver()],
+    })
     // createSvgSpritePlugin({
     //   symbolId: 'icon-[name]-[hash]'
     
